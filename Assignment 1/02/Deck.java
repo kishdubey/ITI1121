f27fc4612cab5be928fa480df59fd2335711f638
Kish Dubey <dkish2000@gmail.com>
2019-03-26 12:42:49 -0400

    Initializing A1

---
+++ Assignment 1/02/Deck.java
@@ -0,0 +1,43 @@
+/**
+* Starting point for your implementation of the class Deck.
+*
+* @author Marcel Turcotte (marcel.turcotte@uottawa.ca)
+*/
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+
+    private ArrayList<Card> cards;
+
+    /**
+    * A constructor for the class <code>Deck</code>. It creates the initial
+    * <code>ArrayList</code> that will be used to store the cards of Otherwise
+    * deck.
+    */
+
+    public Deck() {
+        cards = new ArrayList<Card>();
+    }
+
+    /**
+    * A constructor for the class <code>Deck</code>. It creates the initial
+    * <code>ArrayList</code> that will be used to store the cards of Otherwise
+    * deck. The parameter specifies the number of ranks for the cards. Finally,
+    * it also initializes this deck to contain 4 x n cards, where n is the value
+    * of the parameter.
+    *
+    * @param range the number of ranks for the cards
+    */
+
+
+    public Deck(int range) {
+
+        // Complete the implementation of this constructor
+
+    }
+
+    // Add all the necessary methods here
+
+}
