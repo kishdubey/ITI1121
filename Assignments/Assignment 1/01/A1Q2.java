f40a84e7c54c20f7550e55d5a32da481f7383682
Kish Dubey <44308612+kishdubey@users.noreply.github.com>
2019-05-18 18:40:47 -0400

    Update A1Q2.java

--- Assignments/Assignment 1/01/A1Q2.java
+++ Assignments/Assignment 1/01/A1Q2.java
@@ -1,19 +1,12 @@
 //It checks for the duplicate value in the array
-
 public class A1Q2{
   public static boolean hasLengthTwoRun(double[] array){
   for (int i=0;i<array.length-1;i++){
     if(array[i]==array[i+1])
-    {return true;
+    {
+      return true;
     }
-  }return false;
-
-
   }
-
-
-
-
-
-
+    return false;
+ }
 }
